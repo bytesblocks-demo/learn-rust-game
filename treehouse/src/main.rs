28be21b87a68dcb6d65509a1d38583eb63e97fe7
scottluo <luozhidong9412@outlook.com>
2025-06-11 16:56:08 +0800

    Add Visitor struct with custom greetings Add Visitor struct with custom greetings
    Implements a new Visitor type to encapsulate guest data and greeting
    messages. Adds a loop to continuously process visitors and display the
    final list when done.

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -6,20 +6,47 @@ fn what_is_your_name() -> String {
     name.trim().to_lowercase()
 }
 
-fn main() {
-    println!("Hello, what's your name?");
-    let your_name = what_is_your_name();
-    let visitor_list = ["bert", "steve", "fred"];
-    let mut allow_theme_in = false;
-    for visitor in visitor_list {
-        if visitor == your_name {
-            allow_theme_in = true;
-            break;
+#[derive(Debug)]
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Visitor {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
         }
     }
-    if allow_theme_in {
-        println!("Welcome, {}!", your_name);
-    } else {
-        println!("Sorry, {}! You are not allowed in.", your_name);
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
+fn main() {
+    let mut visitor_list = vec![
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi Steve.Your milk is in the fridge."),
+        Visitor::new("fred", "Wow, who invited Fred?"),
+    ];
+    loop {
+        println!("Hello, what's your name?");
+        let name = what_is_your_name();
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    println!("The final list of visitors:");
+                    println!("{:#?}", visitor_list);
+                    break;
+                } else {
+                    println!("{} is not on the visitor list.", name);
+                    visitor_list.push(Visitor::new(&name, "New Friend"));
+                }
+            }
+        }
     }
 }
