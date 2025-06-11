6840fa1d9c25165fc6c005307922ce012082c8cb
scottluo <luozhidong9412@outlook.com>
2025-06-11 16:20:48 +0800

    Add visitor list check and access control

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -3,11 +3,23 @@ use std::io::stdin;
 fn what_is_your_name() -> String {
     let mut name = String::new();
     stdin().read_line(&mut name).expect("Failed to read line");
-    name.trim().to_string()
+    name.trim().to_lowercase()
 }
 
 fn main() {
     println!("Hello, what's your name?");
     let your_name = what_is_your_name();
-    println!("Hello, {}!", your_name.trim());
+    let visitor_list = ["bert", "steve", "fred"];
+    let mut allow_theme_in = false;
+    for visitor in visitor_list {
+        if visitor == your_name {
+            allow_theme_in = true;
+            break;
+        }
+    }
+    if allow_theme_in {
+        println!("Welcome, {}!", your_name);
+    } else {
+        println!("Sorry, {}! You are not allowed in.", your_name);
+    }
 }
